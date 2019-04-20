8de3e26cca48e1bf2e2e05c8b13414b78d68d445
Lisa Leslie <lisar@ibouge.com>
2019-04-20 14:41:01 -0700

    final connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Redmond2017',
-    database: 'burgers_db'
-})
-
+var connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Redmond2017',
+        database: 'burgers_db'
+    })
+}
 connection.connect(function (err) {
     if (err) throw err;
     console.log('connected as id: ' + connection.threadId);
