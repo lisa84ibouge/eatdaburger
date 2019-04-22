01b20089bc9b613a2622d7e081272c302b5b2199
Lisa Leslie <lisar@ibouge.com>
2019-04-22 15:13:09 -0700

    update schema and seeds

---
+++ database/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `burgers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(255) NOT NULL,
+  `devoured` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
+
+
---
+++ database/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO `burgers` VALUES (1,'Cheese Burger',1),(2,'Lobster Roll',1),(3,'Junior Bacon Cheeseburger',1),(4,'Heritage Burger',1),(5,'The Commons Burger',1),(6,'Shake Shack',1);
